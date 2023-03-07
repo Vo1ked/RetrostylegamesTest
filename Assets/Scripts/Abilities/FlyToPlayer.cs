2a1ef8e746a15e053a8333250db68b193b6cbd72
Volked <Volked18@gmail.com>
2023-03-07 16:05:22 +0200

    convert corutine to async

--- Assets/Scripts/Abilities/FlyToPlayer.cs
+++ Assets/Scripts/Abilities/FlyToPlayer.cs
@@ -1,88 +1,95 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 using Zenject;
 
 [CreateAssetMenu(fileName = "FlyToPlayer", menuName = "My Game/Ability/FlyToPlayer")]
-public class FlyToPlayer : Ability, IPauseHandler {
+public class FlyToPlayer : Ability, IPauseHandler
+{
     public override Specialization Specialization => Specialization.Move;
     public override WorkType WorkType => WorkType.@override;
 
-	[SerializeField] private float _flyUpSpeed;
+    [SerializeField] private float _flySpeed;
 
-	private Player _player;
-	private List<IMovable> _movables = new List<IMovable>();
-	private CoroutineRunner _coroutineRunner;
-	private PauseManager _pauseManager;
-	[Inject]
-	private void Construct(CoroutineRunner coroutineRunner, PauseManager pauseManager, Player player)
-	{
-		_coroutineRunner = coroutineRunner;
-		_pauseManager = pauseManager;
+    private CancellationTokenSource _flyCanceletionToken;
+    private List<IMovable> _movables = new List<IMovable>();
+
+    private Player _player;
+    private PauseManager _pauseManager;
+    [Inject]
+    private void Construct( PauseManager pauseManager, Player player)
+    {
+        _pauseManager = pauseManager;
         _player = player;
-		_pauseManager.SubscribeHandler(this);
+        _flyCanceletionToken = new CancellationTokenSource();
+        _pauseManager.SubscribeHandler(this);
+        _player.OnPlayerDestroy += OnPlayerDestroy;
+    }
 
-		_player.OnPlayerDestroy += OnPlayerDestroy;
-	}
+    private void OnPlayerDestroy()
+    {
+        _player.OnPlayerDestroy -= OnPlayerDestroy;
+        _movables.Clear();
+        _pauseManager.UnsubscribeHandler(this);
+        _flyCanceletionToken.Cancel();
+        _flyCanceletionToken.Dispose();
+    }
 
-	private void OnPlayerDestroy()
-	{
-		_player.OnPlayerDestroy -= OnPlayerDestroy;
-		_movables.Clear();
-		_pauseManager.UnsubscribeHandler(this);
-	}
+    public override void Execute(GameObject user, params object[] parameters)
+    {
+        var movable = user.GetComponent<IMovable>();
+        Fly(movable, _flyCanceletionToken.Token);
+        _movables.Add(movable);
+    }
 
-	public override void Execute(GameObject user, params object[] parameters)
-	{
-		var movable = user.GetComponent<IMovable>();
-		movable.Coroutine = _coroutineRunner.RunCoroutine(Fly(movable));
-		_movables.Add(movable);
-	}
+    private async void Fly(IMovable movable, CancellationToken token)
+    {
+        if (token.IsCancellationRequested)
+        {
+            return;
+        }
+        var direction = (_player.transform.position - movable.Rigidbody.position).normalized;
+        movable.Rigidbody.MovePosition(movable.Rigidbody.position + direction * _flySpeed * Time.deltaTime);
 
-	private IEnumerator Fly(IMovable movable)
-	{
-		if (_pauseManager.IsPaused)
-		{
-			yield break;
-		}
-		var direction = (_player.transform.position - movable.Rigidbody.transform.position).normalized;
-		movable.Rigidbody.MovePosition(movable.Rigidbody.transform.position + direction * _flyUpSpeed * Time.deltaTime);
+        await Task.Delay(Mathf.RoundToInt(Time.fixedDeltaTime * 1000f), token).ContinueWith(IgnoreCanceledTask);
 
-		yield return new WaitForFixedUpdate();
+        if (token.IsCancellationRequested)
+        {
+            return;
+        }
+        Fly(movable, token);
+    }
 
-		if (movable.Coroutine != null)
-		{
-			_coroutineRunner.StopRunningCoroutine((movable.Coroutine));
-			movable.Coroutine = null;
-		}
-		movable.Coroutine = _coroutineRunner.RunCoroutine(Fly(movable));
-	}
+    public void OnPause(bool IsPause)
+    {
+        if (_movables.Count < 1)
+            return;
 
-	public void OnPause(bool IsPause)
-	{
-		if (_movables.Count < 1)
-			return;
+        if (IsPause)
+        {
+            _flyCanceletionToken.Cancel(false);
+            _flyCanceletionToken.Dispose();
+        }
+        else
+        {
+            _flyCanceletionToken = new CancellationTokenSource();
+            foreach (IMovable movable in _movables)
+            {
+                Fly(movable, _flyCanceletionToken.Token);
+            }
+        }
+    }
+    private static void IgnoreCanceledTask(Task task)
+    {
+        if (task.IsCanceled)
+        {
+            return;
+        }
 
-		if (IsPause)
-		{
-			foreach (IMovable movable in _movables)
-			{
-				if (movable.Coroutine != null)
-				{
-					_coroutineRunner.StopRunningCoroutine(movable.Coroutine);
-					movable.Coroutine = null;
-				}
-			}
-		}
-		else
-		{
-			foreach (IMovable movable in _movables)
-			{
-				if (movable.Coroutine == null)
-				{
-					movable.Coroutine = _coroutineRunner.RunCoroutine(Fly(movable));
-				}
-			}
-		}
-	}
+        if (task.Exception != null)
+        {
+            Debug.LogException(task.Exception);
+        }
+    }
 }
